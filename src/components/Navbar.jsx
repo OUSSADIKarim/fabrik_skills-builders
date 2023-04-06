64e0e48c822652efa3732fc4d095614e1ea3b7a0
sofianso24 <sofiane.bahmed.fabrikademy@gmail.com>
2023-04-06 22:47:09 +0200

    finish navbar + hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,33 +7,31 @@ import "../components/NavbarStyles.css";
 
 
 const Navbar = () => {
-  const handleMenue = () => {
-  const menuBtn = document.querySelector(".menue-btn");
-  const navbar = document.querySelector(".navbar");
-  menuBtn.classList.toggle("active");
-  navbar.classList.toggle("active");
-  }
-  // const menuBtn = document.querySelector(".menue-btn");
-  // const navbar = document.querySelector(".navbar");
-  // console.log(menuBtn);
-  // menuBtn.addEventListener("click", () => {
-  //   menuBtn.classList.toggle("active");
-  //   navbar.classList.toggle("active");
-  // })
-  return (
-    <header>
-      <a href="" className="logo"><img src={Logo} /></a>
-      <div className="menue-btn" onClick={handleMenue}><img src={menu} alt="" /> </div>
-      <div className="navbar">
-        <div className="navbar-items">
-          <a href="index.html">HOMME</a>
-          <a href="blog.html">BLOG</a>
-          <a href="destination.html">DESTINATIONS</a>
-          <a href="notfound.html">CONTACT</a>
 
-        </div>
-      </div>
-    </header>
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+
+  return (
+    
+    <div class="topnav" id="myTopnav">
+      <img src={Logo}  />
+      <a href="#home" class="active">Home</a>
+      <a href="#news">HOMME</a>
+      <a href="#contact">COURSES</a>
+      <a href="#about">EVENTS</a>
+      <a href="#about">CALENDAR</a>
+      <a href="#about">JOBS</a>
+      <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fa fa-bars"></i>
+      </a>
+    </div>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,20 +7,24 @@ import heroImg from "../../assets/OBJECTS.png";
 const Home = () => {
   return (
     <div className="hero">
-      <div className="content">
-        <h1>The Smart
-          Choice For Future</h1>
-        <p>skillsbuildes is a global training provider based across
-          the UK <br /> that specialises in accredited and bespoke training courses. We crush the...</p>
-        <form action="https://www.google.com/search" method="get" className="search-bar">
-          <input type="text" placeholder="search anything" name="q" />
-          <button type="submit"><img src={searchImg} /></button>
-        </form>
+      <div className="container">
+        <div className="content">
+          <h1>The Smart
+            Choice For Future</h1>
+          <p>skillsbuildes is a global training provider based across
+            the UK <br /> that specialises in accredited and bespoke training courses. We crush the...</p>
+          <form action="https://www.google.com/search" method="get" className="search-bar">
+            <input type="text" placeholder="search anything" name="q" />
+            <button type="submit"><img src={searchImg} /></button>
+          </form>
+        </div>
+        <div className="hero-img">
+          <img src={heroImg} />
+        </div>
       </div>
-      <div className="hero-img">
-        <img src={heroImg} />
-      </div>
-    </div>)
+
+    </div>
+  )
 
 };
 
