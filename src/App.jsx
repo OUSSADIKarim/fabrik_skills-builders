e23bbee54ab9a18a27ebedcc397d29e65a3ebda3
OUSSADI Karim <oussadikarim00@gmial.com>
2023-04-06 12:41:13 +0100

    data imgs corrected + jobs + events added

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,32 @@
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Courses from "./pages/courses/Courses";
-import Footer from "./components/Footer"
-import Navbar from "./components/Navbar"
-import Home from "./pages/home/Home"
-import Events from "./pages/events/Events"
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./pages/home/Home";
+import Events from "./pages/events/Events";
 
-import {Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import CalendarCourses from "./pages/calendar/CalendarCourses";
+import Jobs from "./pages/jobs/Jobs";
 
 function App() {
   const client = new QueryClient();
   return (
-      <>
-        <QueryClientProvider client={client}>
-        <Navbar/>
-          <Routes>
-              <Route path="/" element={<Home/>} />
-              <Route path="/events" element={<Events/>} />
-              <Route path="/courses" element={<Courses/>} />
-              <Route path="/calendar" element={<CalendarCourses/>} />
-          </Routes>
-        <Footer/>
-        </QueryClientProvider>
-      </>
-  )
+    <>
+      <QueryClientProvider client={client}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/calendar" element={<CalendarCourses />} />
+        </Routes>
+        <Footer />
+      </QueryClientProvider>
+    </>
+  );
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,37 @@
-import React from 'react'
-import  {useState, useEffect} from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
 
-import "../components/NavbarStyles.css"
+import "../components/NavbarStyles.css";
 export default function Navbar() {
-    const [toggleMenu, setToggleMenu] = useState(false)
-    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
-    useEffect(() => {
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    };
 
-        const changeWidth = () => {
-          setScreenWidth(window.innerWidth);
-        }
-    
-       
-        return () => {
-            window.addEventListener('resize', changeWidth)
-            window.removeEventListener('resize', changeWidth)
-        }
-    
-      }, [])
-      const toggleNav = () => {
-        setToggleMenu(!toggleMenu)
-      }
+    return () => {
+      window.addEventListener("resize", changeWidth);
+      window.removeEventListener("resize", changeWidth);
+    };
+  }, []);
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu);
+  };
   return (
     <nav>
       {(toggleMenu || screenWidth > 500) && (
-      <ul className="list">
-      <li className="items">Home</li>
-      <li className="items">Services</li>
-      <li className="items">Contact</li>
-    </ul>
+        <ul className="list">
+          <li className="items">Home</li>
+          <li className="items">Services</li>
+          <li className="items">Contact</li>
+        </ul>
       )}
 
-      <button onClick={toggleNav} className="btn">BTN</button>
+      <button onClick={toggleNav} className="btn">
+        BTN
+      </button>
     </nav>
-  )
+  );
 }
